aefece7ed02289ca0a303c6b4f4c6091a064eacd
SanduniAnuradi <157880699+SanduniAnuradi@users.noreply.github.com>
2026-05-11 22:32:45 +0530

    Add files via upload

---
+++ 1st python.py
@@ -0,0 +1,38 @@
+print("Sanduni Anuradi")
+print("H/Debarawewa National College")
+#This is a single line comment
+"""
+This is a
+multi-line comment
+"""
+print("22 divided by 7 is:",22/7)
+
+operation ="Sum"
+total=8
+print(operation + " is "+ str(total))
+
+length= 10
+width = 5
+area = length*width
+print("the area of rectangle with length" + str(length)+" and width"+str(width) + " is " +str(area))
+
+name = input("Enter your name:")
+print("Hello"+ name + ", welcome to NSBM!")
+print(type(name))
+
+birth_year= input("Enter your birth year:")
+birth_year= int(birth_year)
+print("Your birth year is:"+str(birth_year))
+num = int(5)
+print(type(birth_year))
+
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+num1
+print("Addition:",num1+num2)
+print("Subtraction:", num1 -num2)
+print("Multiplication:", num1*num2)
+print("Division:",num1/num2)
+print("Power", num1**num2)
+
+     
